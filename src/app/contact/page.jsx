c445ba6c3945978194d2e4aaa2680693355291e0
chikafx <kingsobi70@yahoo.com>
2024-09-14 13:00:26 +0100

    feat:changes made on the page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,10 +7,10 @@ export default function Contact() {
       <h2 className='flex justify-around items-center text-yellow-400 text-2xl capitalize '>
       Just say Hey!
       </h2>
-      <h1 className='flex justify-start p-5 ml-5 text-7xl font-mono'>
+      <h1 className='flex justify-start  ml-5 text-7xl font-mono'>
       Send us
       </h1>
-      <h1 className='flex justify-start p-5 ml-5 text-7xl font-mono'>
+      <h1 className='flex justify-start ml-5 text-7xl font-mono'>
       an Email.
       </h1>
       <p>
@@ -20,28 +20,28 @@ export default function Contact() {
       <form>
 
       <fieldset>
-        <ul className='flex p-28 items-center text-center'>
+        <ul className='flex-auto mt-14 items-center text-center'>
           <li className='ml-8 text-2xl'>
-            <label for='name'>Full name</label>
-            <input className='outline-dashed mt-11 text-black ml-3 text-center text-sm' placeholder='enter your name' type="text" name='name' id='fullname' />
+            <label for='name'>Full name:</label>
+            <input className='outline-dashed  text-black ml-3 text-center text-sm' placeholder='enter your name' type="text" name='name' id='fullname' />
           </li>
 
           <li>
-            <label For="email">Email</label>
-            <input  className='outline-dashed text-black mt-11 text-sm text-center required:' placeholder='enter your email' type="email" name='email' id='email' />
+            <label For="email">Email:</label>
+            <input  className='outline-dashed text-black mt-11 ml-3 text-sm text-center required:' placeholder='enter your email' type="email" name='email' id='email' />
           </li>
 
           <li>
-            <label For="phone">Phone Number</label>
+            <label For="phone">Phone Number:</label>
             <input  className='outline-double mt-11 text-black ml-3 text-center text-sm' placeholder='enter phone number' type="tel" name='phone-number' id='phone-number'/>
           </li>
 
           <li>
-            <label For="message">Message</label>
-            <input className='outline-dashed h-36 w-3/5 ml-3 text-center text-black text-sm' placeholder='Go on!!' type="text" name='message' id='message' />
+            <label For="message">Message:</label>
+            <input className='outline-dashed h-36 w-3/5 ml-3 mt-9 text-black text-sm text-center' placeholder='Go on!!' type="text" name='message' id='message' />
           </li>
         </ul>
-        <input className='ml-96 justify-center cursor-pointer' type="submit" value="submit" />
+        <input className='ml-96 justify-center cursor-pointer mt-9' type="submit" value="submit" />
       </fieldset>
       </form>
     </div>
